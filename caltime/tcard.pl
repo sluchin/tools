c199d677f994bda0d7be3ccd80711c5e18d6c22e
Tetsuya Higashi <berio.sequenza@gmail.com>
2013-09-09 20:53:52 +0900

    modified start option

--- caltime/tcard.pl
+++ caltime/tcard.pl
@@ -26,10 +26,11 @@ our $VERSION = do { my @r = ( q$Revision: 0.01 $ =~ /\d+/g );
 };
 
 my $progname = basename($0);
-my $progpath;
-BEGIN { $progpath = dirname($0); }
-use lib "$progpath/lib";
-use gui;
+
+#my $progpath;
+#BEGIN { $progpath = dirname($0); }
+#use lib "$progpath/lib";
+#use gui;
 
 # ステータス
 my %stathash = (
@@ -58,7 +59,7 @@ my %opt = (
 # バージョン情報表示
 #
 sub print_version() {
-    print "$progname version " 
+    print "$progname version "
       . $VERSION . "\n"
       . "  running on Perl version "
       . join( ".", map { $_ ||= 0; $_ * 1 } ( $] =~ /(\d)\.(\d{3})(\d{3})?/ ) )
@@ -126,26 +127,36 @@ my $start    = "出社";
 my $stop     = "退社";
 my $download = "ダウンロード";
 my $url      = "http://itec-hokkaido.dn-cloud.com/";
-my $home     = $url . "cgi-bin/dneo/dneo.cgi";
+my $home     = $url . "cgi-bin/dneo/dneo.cgi?";
 my $login    = $url . "cgi-bin/dneo/dneo.cgi?cmd=login";
 my $tcardlnk = "ztcard.cgi?cmd=tcardindex";
-my $tcard    = $url . "cgi-bin/dneo/" . $tcardlnk;
+
+#my $tcard    = $url . "cgi-bin/dneo/" . $tcardlnk;
+my $tcard = $url . "cgi-bin/dneo/zrtcard.cgi";
 
 my ( undef, undef, undef, undef, $mon, $year ) = localtime(time);
 my $month = sprintf( "%04d%02d", $year + 1900, $mon + 1 );
 my $filename = $opt{'dir'} . "/" . $month . ".csv";
 
 # cookie_jarの生成
-my $cookie_jar = HTTP::Cookies->new(file => "cookie.txt", autosave => 1, ignore_discard => 1);
+my $cookie_jar = HTTP::Cookies->new(
+    file           => "cookie.txt",
+    autosave       => 1,
+    ignore_discard => 1
+);
 my $mech = WWW::Mechanize->new( autocheck => 1, cookie_jar => $cookie_jar );
 
+my $session_id;
+my $token;
+my $json;
+
 sub login {
-    $mech->agent_alias( 'Linux Mozilla' );
+    $mech->agent_alias('Linux Mozilla');
     $mech->get($login);
-    print $mech->content if $opt{'vorbis'};
+    print encode( $enc, $mech->content ) if $opt{'vorbis'};
 
     $mech->submit_form(
-        fields      => {
+        fields => {
             cmd     => 'certify',
             nexturl => 'dneo.cgi?cmd=login',
             UserID  => $opt{'id'},
@@ -154,58 +165,96 @@ sub login {
         },
     );
     die "Can't even get the home page: ", $mech->response->status_line
-        unless $mech->success;
-    print $mech->content if $opt{'vorbis'};
-    my $json = JSON->new()->decode( $mech->content );
+      unless $mech->success;
+    print encode( $enc, $mech->content ) if $opt{'vorbis'};
+    $json = decode_json( encode( 'utf8', $mech->content ) )
+      or die "malformed JSON string: $!: ", encode( 'utf8', $mech->content );
 
-    my $cookie =
-          "dnzSid="   . $json->{'rssid'}  || "" . ";"
-        . "dnzToken=" . $json->{'STOKEN'} || "" . ";"
-        . "dnzSv="    . $json->{'dnzSv'}  || "" . ";";
-    print $cookie if $opt{'vorbis'};
-    $mech->add_header( Cookie => $cookie );
+    $session_id =
+        "dnzSid="
+      . ( encode( 'utf8', $json->{'rssid'} ) || "" ) . ";"
+      . "dnzToken="
+      . ( encode( 'utf8', $json->{'STOKEN'} ) || "" ) . ";"
+      . "dnzSv="
+      . ( encode( 'utf8', $json->{'dnzSv'} ) || "" ) . ";"
+      . "dnzInfo="
+      . ( encode( 'utf8', $json->{'id'} ) || "" );
+    $token = $json->{'STOKEN'};
+    print $session_id . "\n" if $opt{'vorbis'};
+    map { print encode( 'utf8', "$_ => $json->{$_}\n" ) } keys $json;
+
+    print "dnzSid=" .   ( encode( 'utf8', $json->{'rssid'} )  || "" ) . "\n";
+    print "dnzToken=" . ( encode( 'utf8', $json->{'STOKEN'} ) || "" ) . "\n";
+    print "dnzSv=" .    ( encode( 'utf8', $json->{'dnzSv'} )  || "" ) . "\n";
+
+    $mech->add_header( Cookie => $session_id );
     $mech->get($login);
     die "Can't even get the home page: ", $mech->response->status_line
-        unless $mech->success;
-    print $mech->content if $opt{'vorbis'};
+      unless $mech->success;
+    print encode( $enc, $mech->content ) if $opt{'vorbis'};
 }
 
 sub tcard {
     login();
     $mech->follow_link( url => $tcardlnk );
-    print $mech->content if $opt{'vorbis'};
+    print encode( $enc, $mech->content ) if $opt{'vorbis'};
 }
 
 sub start {
     login();
-    $mech->submit_form(
-        fields   => {
-            multicmd => "{\"0\":{\"cmd\":\"tcardcmdstamp\",\"mode\":\"go\"},\"1\":{\"cmd\":\"tcardcmdtick\"}}"
-        },
+    $mech->add_header(
+        Accept          => 'application/json,text/javascript,*/*',
+        Referer         => $home,
+        Cookie          => $session_id,
+        Connection      => 'keep-alive',
+        Pragma          => 'no-cache',
+        'Cache-Control' => 'no-cache'
     );
-    print $mech->content if $opt{'vorbis'};
+    my $response = $mech->post(
+        $tcard,
+        [
+            multicmd =>
+"{\"0\":{\"cmd\":\"tcardcmdstamp\",\"mode\":\"go\"},\"1\":{\"cmd\":\"tcardcmdtick\"}}",
+            $token => 1
+        ]
+    );
+    print encode( $enc, $mech->content ) if $opt{'vorbis'};
+    exit( $stathash{'EX_OK'} );
 }
 
 sub stop {
     login();
-    $mech->submit_form(
-        fields   => {
-            multicmd => "{\"0\":{\"cmd\":\"tcardcmdstamp\",\"mode\":\"leave\"},\"1\":{\"cmd\":\"tcardcmdtick\"}}"
-        },
+    $mech->add_header(
+        Accept          => 'application/json,text/javascript,*/*',
+        Referer         => $home,
+        Cookie          => $session_id,
+        Connection      => 'keep-alive',
+        Pragma          => 'no-cache',
+        'Cache-Control' => 'no-cache'
     );
-    print $mech->content if $opt{'vorbis'};
+    my $response = $mech->post(
+        $tcard,
+        [
+            multicmd =>
+"{\"0\":{\"cmd\":\"tcardcmdstamp\",\"mode\":\"leave\"},\"1\":{\"cmd\":\"tcardcmdtick\"}}",
+            $token => 1
+        ]
+    );
+    print encode( $enc, $mech->content ) if $opt{'vorbis'};
+    exit( $stathash{'EX_OK'} );
 }
 
 sub download {
     tcard();
     $mech->submit_form(
-        fields   => {
+        fields => {
             cmd  => 'tcardcmdexport',
             date => $month . "01",
         },
     );
     print encode( $enc, decode( $dec, $mech->content ) ) if $opt{'vorbis'};
     $mech->save_content($filename);
+    exit( $stathash{'EX_OK'} );
 }
 
 sub tk_window {
@@ -223,18 +272,18 @@ sub tk_window {
     MainLoop();
 }
 
-
 if ( $opt{'start'} ) {
     tk_window( "Go", \&start ) unless ( $opt{'nogui'} );
     start();
-} elsif ( $opt{'stop'} ) {
+}
+elsif ( $opt{'stop'} ) {
     tk_window( "Leave", \&stop ) unless ( $opt{'nogui'} );
     stop();
-} else {
+}
+else {
     tk_window( "Download", \&download ) unless ( $opt{'nogui'} );
     download();
 }
-
 
 exit( $stathash{'EX_OK'} );
 
